c32f13222d00ea48c2bbbaa7f16e474a9a5c31d2
Adam Fryman <nerionx@googlemail.com>
2016-12-14 15:10:02 +0000

    Split update and show into seperate functions
    allows for calling of fps variable to display however you like rather
    than the default text

--- fps.js
+++ fps.js
@@ -1,8 +1,13 @@
-/* Simple FPS counter for J5, add it to code and create and object equal to FPS, then call SHOW in the draw function
+/* Simple FPS counter for J5, add it to code and create and object equal to FPS, then call "Update" and "Show" in the draw function
 of the main program e.g.
   fps = new FPS;
   
   fps.Show(x,y);
+
+  The current fps can be accessed via the fps variable for displaying via other means e.g. 
+  
+  fps = new FPS
+  console.log(fps.fps)
 */
 
 
@@ -11,7 +16,7 @@ function FPS(){
   this.frameCounter = 0;
   this.fps = 0;
   this.oldTime = millis();
-  this.Show=function(x,y){
+  this.Update=function(){
     if(millis() - this.oldTime < 1000){
       this.frameCounter +=1;
     }else{
@@ -19,7 +24,8 @@ function FPS(){
       this.frameCounter = 0;
       this.oldTime = millis()
     }
-    text(this.fps,x,y);
-  
 }
+  this.Show=function(x,y){
+    text(this.fps,x,y);
+  }
 }
